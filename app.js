fc21ef0dfb602d835778f386c96d3ca5382cc8dd
slap-fi <69651512+slap-fi@users.noreply.github.com>
2021-08-05 02:28:10 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const userAction = async () => {
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=GZDrXLQhN96rrD5lTopVQMhgJ61ekWfQRSchiIxV');
+    const myJson = await response.json(); //extract JSON from the http response
+
+    document.querySelector("#root").innerHTML += `<h1 class="title">${myJson.title}</h1>`
+    document.querySelector("#root").innerHTML += `<h2 class="date">${myJson.date}</h2>`
+    document.querySelector("#root").innerHTML += `<img class="hdurl" src=${myJson.hdurl}>`   
+    document.querySelector("#root").innerHTML += `<p class="explanation">${myJson.explanation}</p>`
+    document.querySelector("#copy").innerHTML += `<p class="explanation">${myJson.copyright}</p>`
+}
+
+userAction()
